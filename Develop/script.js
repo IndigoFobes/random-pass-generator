cd34ce3c223986140e0a27972d536a423f4551b5
Indigo <indigo.crandell@gmail.com>
2022-07-21 14:30:08 -0400

    Added some notes for clarity.

--- Develop/script.js
+++ Develop/script.js
@@ -10,7 +10,7 @@ var userChoice = {
   // Arrays for each key-value listed above
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-//var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] 
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0] 
 var special = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
 
 
@@ -18,16 +18,15 @@ var special = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", 
 var generateBtn = document.querySelector("#generate");
 
 
-
 // Write password to the #password input
-  //generatePassword function needs to be defined
-
 function writePassword() {
   var password = generatePassword()
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
+
+// HERE'S THE BIG IF/ELSE FUNCTION
 //Define generatePassword
 function generatePassword() {
   var userLength = window.prompt("Please enter how long you would like your password to be. It must be between 8 and 128 characters.");
@@ -37,6 +36,7 @@ function generatePassword() {
     return;
   }
 
+  // If the user selects a number between 8 and 128, we are good to move forward and inquire about uppercase letters. Otherwise, return.
   if (userLength >= 8 && userLength <= 128) {
     var userUpper = window.confirm("Please confirm: Would you like to include uppercase letters in your password?");
   }
@@ -44,31 +44,27 @@ function generatePassword() {
     return;
   };
 
+  // Whether or not the user includes uppercase, ask about lowercase next...
   if (userUpper == true || userUpper == false) {
     var userLower = window.confirm("Please confirm: Would you like to include lowercase letters in your password?");
   }
-  else {
-    return;
-  }
 
+  // Whether or not the user includes lowercase, ask about numbers...
   if (userLower == true || userLower == false) {
     var userNumbers = window.confirm("Please confirm: Would you like to include numbers in your password?");
   }
-  else {
-    return;
-  }
 
+  // Whether or not the user includes numbers, ask about special characters...
   if (userNumbers == true || userNumbers == false) {
     var userSpecial = window.confirm("Please confirm: Would you like to include special characters in your password?");
   }
-  else {
-    return;
-  }
 
+  // If the user selects 'cancel' for each character type, they will be asked to try again.
   if (userUpper == false && userLower == false && userNumbers == false && userSpecial == false) {
     var errorMessage = window.confirm("You must select at least on kind of character to receive a password. Please try again.")
   }
 
+  // They either start over or return to the main screen.
   if (errorMessage == true) {
     return generatePassword();
   }
@@ -76,20 +72,10 @@ function generatePassword() {
     return;
   }
 
-  //
-//function userUpperYes(result) {
- // var userLower = window.prompt("Do you want lowercase, too??");
-//};
-
-//function userUpperNo(error) {
-//  function userLower()
-//}
-
-//function userLower.then(userUpperYes, userUpperNo);
+// And now the math!
 
 
 }
-
 
 
 
@@ -97,8 +83,6 @@ function generatePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
 
 
 
